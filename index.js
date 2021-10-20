0bbf371593641afb068268773f4036b3fa649bf6
shundroid <shundroid@users.noreply.github.com>
2021-10-20 20:39:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ hexo.extend.filter.register('before_post_render', function(data) {
 // Add CDN CSS resources
 hexo.extend.filter.register('after_post_render', function(data) {
   data.content =
-      util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/hint.css/2.4.1/hint.min.css'}) +
+      util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: '/assets/theme/reference/hint.min.css'}) +
       data.content;
   return data;
 });
